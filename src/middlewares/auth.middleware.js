aca1420b342d3fc541be3b8b1c0e157193127588
Teresa <teresabroyer2003@gmail.com>
2023-09-05 11:17:44 -0500

    middleware auth

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,4 +1,21 @@
+const jwt = require("jsonwebtoken")
+require("dotenv").config()
+
 const authenticate = (req, res, next) => {
-    const token = req.headers.authorization
-    next()
+    const bearerToken = req.headers.authorization
+    if(bearerToken){
+        const token = bearerToken.split("Bearer ")[1]
+        console.log(token);
+        try {
+            const decoded = jwt.verify(token, process.env.SECRET, "HS512",
+            console.log(decoded)) 
+            next()
+        } catch (error) {
+            next({
+                status: 400,
+                errorContent: error,
+                message: "Invalid token"
+            })
+        }
+    }
 }
--- src/routes/vehicles.routes.js
+++ src/routes/vehicles.routes.js
@@ -1,13 +1,13 @@
-// const express = require("express");
-// const router = express.Router();
-// const { getVehicles,getVehicle, createVehicle, editVehicles, deleteVehicle } = require("../controllers");
+const express = require("express");
+const router = express.Router();
+const { getVehicles,getVehicle, createVehicle, editVehicles, deleteVehicle } = require("../controllers");
 
 
 
-// router.get("/vehicle", getVehicles);
-// router.get("/vehicle/:id", getVehicle);
-// router.post("/vehicle", createVehicle);
-// router.patch("/vehicle/:id", editVehicles);
-// router.delete("/vehicle/:id", deleteVehicle);
+router.get("/vehicle", getVehicles);
+router.get("/vehicle/:id", getVehicle);
+router.post("/vehicle", createVehicle);
+router.patch("/vehicle/:id", editVehicles);
+router.delete("/vehicle/:id", deleteVehicle);
 
-// module.exports = router;
+module.exports = router;

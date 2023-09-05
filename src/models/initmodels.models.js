397964f0dc8d76d6713acc16ce4686758b1903f7
J-Daniel-Bedoya <jbedoyachavarriaga@gmail.com>
2023-09-04 20:47:10 -0500

    solución de errores en las tablas relacionadas

--- src/models/initmodels.models.js
+++ src/models/initmodels.models.js
@@ -1,18 +1,14 @@
-const { Buyers, Price, Seles, Users, Vehicle } = require("./index");
+const { Buyers, Price, Sales, Vehicles } = require("./index");
 
 const initModels = () => {
-console.log("hola");
+  Sales.belongsTo(Vehicles, { as: "sales", foreignKey: "vehicles_id" });
+  Vehicles.hasMany(Sales, { as: "vehicles", foreignKey: "vehicles_id" });
 
-    Seles.belongsTo(Vehicle, {as: "seles", foreignKey: "vehicles_id"})
-    Vehicle.hasMany(Seles, {as: "vehicles", foreignKey: "vehicles_id"})
+  Sales.belongsTo(Buyers, { as: "buyer", foreignKey: "saleMade_id" });
+  Buyers.hasMany(Sales, { as: "saleMade", foreignKey: "saleMade_id" });
 
-    Seles.belongsTo(Buyers, {as: "buyer", foreignKey: "seleMade_id"})
-    Buyers.hasMany(Seles, {as: "seleMade", foreignKey: "seleMade_id"})
-
-    Vehicle.belongsTo(Price, {as: "vehicle", foreignKey: "vihicle_id"})
-    Price.hasMany(Vehicle, {as: "price", foreignKey: "vihicle_id"})
-
-
+  Vehicles.belongsTo(Price, { as: "price", foreignKey: "vehicle_id" });
+  Price.hasMany(Vehicles, { as: "price", foreignKey: "vehicle_id" }); 
 };
 
 module.exports = initModels;

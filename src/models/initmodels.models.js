1ce96cc4f4674081c548e2689ae2eff30b1d2576
J-Daniel-Bedoya <jbedoyachavarriaga@gmail.com>
2023-09-05 19:43:42 -0500

    solución de error en initModels

--- src/models/initmodels.models.js
+++ src/models/initmodels.models.js
@@ -3,6 +3,10 @@ const { Buyers, Price, Sales, Vehicles } = require("./index");
 const initModels = () => {
     Buyers.hasMany(Sales,  {as: 'buyer', foreignKey:'buyer_id'})
     Sales.hasOne(Buyers, {as: 'sell', foreignKey:'sale_id'})
+
+    Price,
+    Sales, 
+    Vehicles
 };
 
 module.exports = initModels;
